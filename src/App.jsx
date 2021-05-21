8904cc1267caa60d1cc5f0ef6e9e48e0fe985de7
Taka5890 <40171052+Taka5890@users.noreply.github.com>
2021-05-22 08:43:05 +0900

    propstpstateの概念を抑えた

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+  // stateを利用するためには、useStateで分割代入を利用し、
+  // 1つ目に動的に変わる値の変数,2つ目に動的に変更する関数を指定する
+  const [num, setNum] = useState(0);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちわ</h1>
-      <p style={contentStyle}>お元気ですが？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
+
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
