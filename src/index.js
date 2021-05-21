5ae8f2755558717b39928076ce8b4f26de55c6ad
Taka5890 <40171052+Taka5890@users.noreply.github.com>
2021-05-22 07:24:28 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+import ReactDom from "react-dom";
